d33c69d552d3f58a4001137fe2ac5cdff1b20f3e
WetHorse <s26850@pjwstk.edu.pl>
2024-11-30 17:32:02 +0100

    Player Movement
    Fixed Rotation (While reaches 60 degrees - console message )

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,45 +4,57 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField, Range(0, 100)] private float moveSpeedVertical =10f, moveSpeedHorizontal = 10f, rotationSpeed = 5f, upwardSpeed = 5f;
+    [SerializeField, Range(0, 100)] private float  moveSpeedHorizontal = 10f, tiltSpeed = 22.5f, upwardSpeed = 5f;
     [SerializeField] private float horizontalSmoothFactorTime = 0.1f;
+    [SerializeField] private float tiltRecoverySpeed = 20f;
+    [SerializeField] private float maxTilt = 90f;
     
-    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private new Rigidbody rigidbody;
     [SerializeField] private Transform thrust;
     private DefaultInputActions inputActions;
 
     private float positionX;
     private float currentVelocityX;
+    private float currentTilt;
     
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        
-        inputActions = new DefaultInputActions();
-        inputActions.Player.Enable();
         positionX = transform.position.x;
 
     }
-
-    // private void OnMovedLeft(InputAction.CallbackContext obj)
-    // {
-    //     rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, 0);
-    //     rigidbody.AddForceAtPosition(-transform.right * rotationSpeed, thrust.position, ForceMode.Impulse);
-    // }
-    //
-    // private void OnMovedRight(InputAction.CallbackContext obj)
-    // {
-    //     rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, 0);
-    //     rigidbody.AddForceAtPosition(transform.right * rotationSpeed, thrust.position, ForceMode.Impulse);
-    // }
-
-
+    
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
         positionX += horizontalInput * moveSpeedHorizontal * Time.deltaTime;
+        positionX = Mathf.Clamp(positionX, -10f, 10f);
+        currentTilt += horizontalInput * tiltSpeed * Time.deltaTime;
+
+        if (horizontalInput < 0)
+        {
+            currentTilt = Mathf.MoveTowards(currentTilt, -maxTilt, tiltSpeed * Time.deltaTime);
+        }
+        else if (horizontalInput > 0) 
+        {
+            currentTilt = Mathf.MoveTowards(currentTilt, maxTilt, tiltSpeed * Time.deltaTime);
+        }
+        else
+        {
+            currentTilt = Mathf.MoveTowards(currentTilt, 0, tiltRecoverySpeed * Time.deltaTime);
+        }
+        
+        if (Mathf.Abs(currentTilt) > 60f)
+        {
+            Debug.Log("Rocket critically tilted!");
+        }
+
+        currentTilt = Mathf.Clamp(currentTilt, -maxTilt, maxTilt);
+        
+        positionX += horizontalInput * moveSpeedHorizontal * Time.deltaTime;
+        
         positionX = Mathf.Clamp(positionX, -10f, 10f);
     }
 
@@ -50,7 +62,10 @@ public class PlayerMovement : MonoBehaviour
     {
         float smoothX_Value = Mathf.SmoothDamp(transform.position.x, positionX, ref currentVelocityX,
             horizontalSmoothFactorTime);
-        // rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeedHorizontal,moveSpeedVertical, rigidbody.velocity.z);
+        
         rigidbody.MovePosition(new Vector3(smoothX_Value, transform.position.y + upwardSpeed * Time.fixedDeltaTime, transform.position.z));
+        
+        Quaternion targetRotation = Quaternion.Euler(0, 0, -currentTilt);
+        rigidbody.MoveRotation(targetRotation);
     }
 }
