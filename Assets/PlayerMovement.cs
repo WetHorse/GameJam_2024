7925fc5b9cc7841a782cbc850c5478673f24fed2
Daniil <den.danya.denysov@gmail.com>
2024-11-30 15:31:56 +0100

    Added basic movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,43 +1,44 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField, Range(0,100)] private float moveSpeedVertical, moveSpeedHorizontal, rotationSpeed;
+    [SerializeField, Range(0, 100)] private float moveSpeedVertical, moveSpeedHorizontal, rotationSpeed;
     [SerializeField] private Rigidbody rigidbody;
-
+    [SerializeField] private Transform thrust;
     private DefaultInputActions inputActions;
-    
 
     void Start()
     {
         inputActions = new DefaultInputActions();
         inputActions.Player.Enable();
         inputActions.Player.MoveLeft.performed += OnMovedLeft;
+       // inputActions.Player.MoveLeft.canceled += OnMovedLeft;
         inputActions.Player.MoveRight.performed += OnMovedRight;
+        //inputActions.Player.MoveRight.canceled += OnMovedRight;
     }
 
     private void OnMovedLeft(InputAction.CallbackContext obj)
     {
-        rigidbody.AddForce(transform.right * moveSpeedHorizontal * Time.deltaTime, ForceMode.Impulse);
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, 0);
+        rigidbody.AddForceAtPosition(-transform.right * rotationSpeed, thrust.position, ForceMode.Impulse);
     }
 
     private void OnMovedRight(InputAction.CallbackContext obj)
     {
-        rigidbody.AddForce(-transform.right * moveSpeedHorizontal * Time.deltaTime, ForceMode.Impulse);
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, 0);
+        rigidbody.AddForceAtPosition(transform.right * rotationSpeed, thrust.position, ForceMode.Impulse);
     }
+
 
     void Update()
     {
-        //if (!Input.GetKey(KeyCode.Space)) return;
-        rigidbody.AddForce(Vector3.up * moveSpeedVertical * Time.deltaTime,ForceMode.Impulse);
+     
     }
 
     private void FixedUpdate()
     {
-        transform.rotation = Quaternion.Euler(0, 0, rigidbody.velocity.y * rotationSpeed * Time.deltaTime);
+        rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeedHorizontal,moveSpeedVertical, rigidbody.velocity.z);
     }
 }
